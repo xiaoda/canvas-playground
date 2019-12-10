a799901761a5634556932a6e120ec7b8d2a97647
xiaoda <lqdai.xiaoda@gmail.com>
2019-12-10 16:50:24 +0800

    Complete cars-size

--- cars-size/data.js
+++ cars-size/data.js
@@ -4,22 +4,18 @@ const data = [
     models: [
       {
         model: 'A4L',
-        type: 'Sedan',
         size: [4818, 1843, 1432]
       },
       {
         model: 'A6L',
-        type: 'Sedan',
         size: [5038, 1886, 1475]
       },
       {
         model: 'Q3',
-        type: 'SUV',
         size: [4481, 1848, 1584]
       },
       {
         model: 'Q5L',
-        type: 'SUV',
         size: [4753, 1893, 1659]
       }
     ]
@@ -29,22 +25,18 @@ const data = [
     models: [
       {
         model: '3',
-        type: 'Sedan',
         size: [4719, 1827, 1459]
       },
       {
         model: '5',
-        type: 'Sedan',
         size: [4954, 1868, 1489]
       },
       {
         model: 'X1',
-        type: 'SUV',
         size: [4565, 1821, 1620]
       },
       {
         model: 'X3',
-        type: 'SUV',
         size: [4717, 1891, 1689]
       }
     ]
@@ -54,22 +46,18 @@ const data = [
     models: [
       {
         model: 'IS',
-        type: 'Sedan',
         size: [4680, 1810, 1430]
       },
       {
         model: 'ES',
-        type: 'Sedan',
         size: [4975, 1866, 1447]
       },
       {
         model: 'NX',
-        type: 'SUV',
         size: [4640, 1845, 1645]
       },
       {
         model: 'RX',
-        type: 'SUV',
         size: [4890, 1895, 1710]
       }
     ]
@@ -79,12 +67,10 @@ const data = [
     models: [
       {
         model: 'Q50L',
-        type: 'Sedan',
         size: [4867, 1823, 1456]
       },
       {
         model: 'QX50',
-        type: 'SUV',
         size: [4705, 1903, 1681]
       }
     ]
